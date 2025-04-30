42d6ca40113a77702d63dbb73b1c180ebc063ab2
chandu <gopichandu6080@gmail.com>
2025-04-30 19:26:22 +0530

    setup of ejs template engine

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import ejs from "ejs";
 dotenv.config();
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 const port = process.env.PORT_NUM;
 
 app.get("/", (req, res) => {
-  res.status(200).send("<p>Hello, World</p>");
+  res.status(200).render("home");
 });
 
+app.get("/login", (req, res) => {
+  res.status(200).render("login");
+});
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
