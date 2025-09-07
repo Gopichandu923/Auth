1a25cf2966db67d7e481f5bdc01bbd08bde7ee6a
Gopichandu923 <gopichandu6080@gmail.com>
2025-09-07 21:30:41 +0530

    added the register methods and comments

--- index.js
+++ index.js
@@ -5,26 +5,35 @@ import { connect } from "./databaseconfig.js";
 
 dotenv.config();
 
-const app = express();
+const app = express(); // Intialization of express app
 
-connect();
+connect(); // for database connection
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // setting  view engine
 
 const port = process.env.PORT_NUM;
 
+//home page  - get request
 app.get("/", (req, res) => {
   res.status(200).render("home");
 });
 
-//for login page   - get request
+//to get login page   - get request
 app.get("/login", (req, res) => {
   res.status(200).render("login");
 });
 
-//for login -post request
+//for user login  -post request
 app.post("/login", async (req, res) => {});
 
+//to get register page   - get request
+
+app.get("/register");
+
+//to post data to register user  - post request
+app.post("/register", async (req, res) => {});
+
+//to run the server on port
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
