4359ef82aec2aa152e9f952eefd7f3966c3db782
Gopichandu923 <gopichandu6080@gmail.com>
2025-09-07 20:24:47 +0530

    updated the  connection logic

--- databaseconfig.js
+++ databaseconfig.js
@@ -5,7 +5,9 @@ dotenv.config();
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_LOCAL_URI);
+    await mongoose
+      .connect(process.env.MONGO_LOCAL_URL)
+      .then(() => console.log("Successfully connected to the database"));
   } catch (error) {
     throw Error("Error occured while connecting to database" + error);
   }
